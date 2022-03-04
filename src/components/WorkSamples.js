c73e7a240e23249a641161996b095c3f1782b566
chrismuto <chrismuto@gmail.com>
2022-03-03 20:47:29 -0800

    Added work samples with links and styling

--- src/components/WorkSamples.js
+++ src/components/WorkSamples.js
@@ -3,31 +3,33 @@ import React, { useState } from "react";
 const WorkSamples = () => {
   return (
     <div>
-      <a href="https://protected-taiga-32909.herokuapp.com/" target="_blank">
-        <div id="helpForHire">
-          <p class="wsp">Help for Hire</p>
+        <nav id="workNav">
+      <a className="workCard" id="helpForHire" href="https://protected-taiga-32909.herokuapp.com/" target="_blank">
+        <div>
+          <p className="wsp">Help for Hire</p>
         </div>
       </a>
-      <a href="https://chrismuto.github.io/project-01/" target="_blank">
-        <div id="recipeRoulette">
-          <p class="wsp">Recipe Roulette</p>
+      <a className="workCard"  id="recipeRoulette" href="https://chrismuto.github.io/project-01/" target="_blank">
+        <div>
+          <p className="wsp">Recipe Roulette</p>
         </div>
       </a>
-      <a href="https://chrismuto.github.io/weatherapp/" target="_blank">
-        <div id="weatherApp">
-          <p class="wsp">Weather Forecast</p>
+      <a className="workCard" id="weatherApp" href="https://chrismuto.github.io/weatherapp/" target="_blank">
+        <div>
+          <p className="wsp">Weather Forecast</p>
         </div>
       </a>
-      <a href="https://chrismuto.github.io/work-day-scheduler/" target="_blank">
-        <div id="workScheduler">
-          <p class="wsp">Workday Planner</p>
+      <a className="workCard" id="workScheduler" href="https://chrismuto.github.io/work-day-scheduler/" target="_blank">
+        <div>
+          <p className="wsp">Workday Planner</p>
         </div>
       </a>
-      <a href="https://github.com/chrismuto/commerce-backend" target="_blank">
-        <div className="commerceBackend">
-          <p class="wsp">E-commerce platform</p>
+      <a className="workCard" id="commerceBackend" href="https://github.com/chrismuto/commerce-backend" target="_blank">
+        <div>
+          <p className="wsp">E-commerce platform</p>
         </div>
       </a>
+      </nav>
     </div>
   );
 };
