f697786efe6ae2f2cc1984ddb55777a1de4a25ec
chrismuto <chrismuto@gmail.com>
2022-03-04 17:31:59 -0800

    added linked icons for linkedIn and github profiles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,8 @@ import Resume from "./Resume";
 import About from "./About";
 import WorkSamples from "./WorkSamples";
 import Contact from "./Contact";
+import linkedInlogo from "../../src/linkedIn-logo.png";
+import githublogo from "../../src/GitHub-Mark.png";
 
 const Portfolio = () => {
   const [currentPage, setCurrentPage] = useState("About");
@@ -25,18 +27,18 @@ const Portfolio = () => {
       <Navbar setCurrentPage={setCurrentPage} />
       {renderPage()}
       <footer className="footer">
-        <section className="">
           <ul className="footer-ul">
             <li className="githubLink">
-              <a href="https://github.com/chrismuto">Github</a>
+              <a href="https://github.com/chrismuto">
+              <img src={githublogo} alt="github profile"/>
+              </a>
             </li>
-            <li className="linkedInLink">
+            <li>
               <a href="https://www.linkedin.com/in/chris-muto-8453469/">
-                LinkedIn
+              <img src={linkedInlogo} alt="linkedIn profile"/>
               </a>
             </li>
           </ul>
-        </section>
       </footer>
     </div>
   );
